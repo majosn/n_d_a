301b7991c6c7cf2ec80ac78b71b9211e4804bfa2
majosn <majo.serrano.notario@gmail.com>
2026-03-27 08:23:58 -0600

    hola

---
+++ lib/nda.ex
@@ -0,0 +1,18 @@
+defmodule NDA do
+  @moduledoc """
+  Documentation for `NDA`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> NDA.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/nda_test.exs
@@ -0,0 +1,8 @@
+defmodule NDATest do
+  use ExUnit.Case
+  doctest NDA
+
+  test "greets the world" do
+    assert NDA.hello() == :world
+  end
+end
