973fc15996016d57b5f42394ec7bfe2534c174c2
majosn <majo.serrano.notario@gmail.com>
2026-04-09 12:10:19 -0600

    mix test test/test-nfa.exs

--- lib/nda.ex
+++ lib/nda.ex
@@ -1,18 +1,70 @@
-defmodule NDA do
-  @moduledoc """
-  Documentation for `NDA`.
-  """
+defmodule Nfa do
+#Parámetros
+#q: lista de estados del NFA, [0, 1, 2, 3]
+#sigma: alfabeto [:a, :b]
+#inicio: 0
+# final: [3]
+#delta: mapa de transiciones {estado, símbolo} => [estados], %{{0,:a}=>[0,1], {0,:b}=>[0], {1,:b}=>[2], {2,:b}=>[3]}
 
-  @doc """
-  Hello world.
+#QP - estados AFD
+# sigma_p - alfabeto AFD
+# final_p - aceptados AFD
+# inicio_p - inicial AFD
+# delta_p - transiciones AFD
 
-  ## Examples
+  def determinize(_q, sigma, inicio, final, delta) do
+    inicio_p = MapSet.new([inicio])
+    final_set = MapSet.new(final)
 
-      iex> NDA.hello()
-      :world
+    {qp, delta_p, final_p} =
+      bfs([inicio_p], MapSet.new([inicio_p]), %{}, [], sigma, final_set, delta)
 
-  """
-  def hello do
-    :world
+    {qp, sigma, inicio_p, final_p, delta_p}
+  end
+
+  defp bfs([], visited, delta_p, final_p, _sigma, _final_set, _delta) do
+    {MapSet.to_list(visited), delta_p, final_p}
+  end
+
+  defp bfs([current | rest], visited, delta_p, final_p, sigma, final_set, delta) do
+    {new_delta_p, new_states} =
+      Enum.reduce(sigma, {delta_p, []}, fn symbol, {dp_acc, new_acc} ->
+        target = move(current, symbol, delta)
+
+        updated_delta_p =
+          if MapSet.size(target) > 0 do
+            Map.put(dp_acc, {current, symbol}, target)
+          else
+            dp_acc
+          end
+
+        new_unvisited =
+          if MapSet.size(target) > 0 and not MapSet.member?(visited, target) do
+            [target | new_acc]
+          else
+            new_acc
+          end
+
+        {updated_delta_p, new_unvisited}
+      end)
+
+    new_final_p =
+      if MapSet.disjoint?(current, final_set) do
+        final_p
+      else
+        [current | final_p]
+      end
+
+    new_visited = Enum.reduce(new_states, visited, &MapSet.put(&2, &1))
+    new_queue = rest ++ new_states
+
+    bfs(new_queue, new_visited, new_delta_p, new_final_p, sigma, final_set, delta)
+  end
+
+  defp move(state_set, symbol, delta) do
+    state_set
+    |> MapSet.to_list()
+    |> Enum.flat_map(fn q -> Map.get(delta, {q, symbol}, []) end)
+    |> MapSet.new()
   end
 end
--- test/nda_test.exs
+++ test/nda_test.exs
@@ -1,8 +1,52 @@
-defmodule NDATest do
+defmodule NfaTest do
   use ExUnit.Case
-  doctest NDA
 
-  test "greets the world" do
-    assert NDA.hello() == :world
+  test "Automata no determinista a determinista- fucnion determinize" do
+    q      = [0, 1, 2, 3]
+    sigma  = [:a, :b]
+    inicio = 0
+    final  = [3]
+    delta  = %{
+      {0, :a} => [0, 1],
+      {0, :b} => [0],
+      {1, :b} => [2],
+      {2, :b} => [3]
+    }
+
+    {qp, sigma_p, inicio_p, final_p, delta_p} =
+      Nfa.determinize(q, sigma, inicio, final, delta)
+
+    q0  = MapSet.new([0])
+    q01 = MapSet.new([0, 1])
+    q02 = MapSet.new([0, 2])
+    q03 = MapSet.new([0, 3])
+
+    # El alfabeto no cambia
+    assert sigma_p == [:a, :b]
+
+    # El estado inicial es {0}
+    assert inicio_p == q0
+
+    # Los 4 subconjuntos alcanzables están presentes
+    assert q0  in qp
+    assert q01 in qp
+    assert q02 in qp
+    assert q03 in qp
+
+    # Solo {0,3} es aceptado (intersecta con final del NFA)
+    assert q03 in final_p
+    refute q0  in final_p
+    refute q01 in final_p
+    refute q02 in final_p
+
+    # Transiciones del DFA
+    assert delta_p[{q0,  :a}] == q01
+    assert delta_p[{q0,  :b}] == q0
+    assert delta_p[{q01, :a}] == q01
+    assert delta_p[{q01, :b}] == q02
+    assert delta_p[{q02, :a}] == q01
+    assert delta_p[{q02, :b}] == q03
+    assert delta_p[{q03, :a}] == q01
+    assert delta_p[{q03, :b}] == q0
   end
 end
